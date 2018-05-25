1feef370cc1082d4f10ee2b976c37f6dae2c8d67
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-25 15:12:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-blankBoard =
 board = 
 
-def display_board(board = blankBoard)
+def display_board(board)
   
 end
