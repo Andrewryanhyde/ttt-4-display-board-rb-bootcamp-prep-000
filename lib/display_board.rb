92a77a150c37d8f8b9163d70e085634debffb0a7
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-25 15:11:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-blankBoard = ["   ","|", "   ","|", "   ", "   ","|", "   ","|", "   ","   ","|", "   ","|", "   "]
-board = ["   ","|", "   ","|", "   ", "   ","|", "   ","|", "   ","   ","|", "   ","|", "   "]
+blankBoard =
+board = 
 
 def display_board(board = blankBoard)
   
